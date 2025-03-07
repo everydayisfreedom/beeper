d9a46b055cc0233c17421a5019b0a94a3d97672e
Meryem <76478333+mebenyahia@users.noreply.github.com>
2025-03-07 05:29:54 +0100

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,9 +1,8 @@
 import Beep
 
 while True:
-		text = input('bip ♫ >> ')
-		result, error = Beep.play('<stdin>', text)
-
-		if error: print(error.as_string())
-		else: print(result)
-		
+	text = input('bip ♫ >> ')
+	result, error = Beep.play('<stdin>', text)
+	
+	if error: print(error.as_string())
+	else: print(result)
